b8607822f47917c2587e23f64419f236a897dd6a
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-03 16:43:59 +0200

    added allowed host

--- Blog/settings.py
+++ Blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lit-gorge-59056.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
