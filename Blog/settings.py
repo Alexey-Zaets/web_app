d2c784a89773c707f71774abfb584336b0c7da0f
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-05 20:15:08 +0200

    -

--- Blog/settings.py
+++ Blog/settings.py
@@ -154,7 +154,7 @@ STATICFILES_FINDERS = (
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'Blog.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
