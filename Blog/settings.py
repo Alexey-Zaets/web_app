dceacb12d40e054b795909a9d8bbc4aa130d1e97
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-05 18:12:43 +0200

    -

--- Blog/settings.py
+++ Blog/settings.py
@@ -154,7 +154,7 @@ STATICFILES_FINDERS = (
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
