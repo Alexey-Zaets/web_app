c34856bb7a3aa4584131693b273db92dda1dbbd1
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-05 19:54:44 +0200

    -

--- Blog/storage.py
+++ Blog/storage.py
@@ -1,5 +1,4 @@
 from whitenoise.storage import CompressedManifestStaticFilesStorage
-from django.contrib.staticfiles.storage import ManifestFilesMixin
 
 class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    ManifestFilesMixin.manifest_strict = False 
+	super().manifest_strict = False 
