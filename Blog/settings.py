7a1172fd7244a0a2009f15f74b346185fd5abc9e
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-10 22:17:07 +0200

    -

--- Blog/settings.py
+++ Blog/settings.py
@@ -163,7 +163,7 @@ STATICFILES_FINDERS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
