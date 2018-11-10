176b652282df71864caaf1aa1730eea460db3bb1
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-10 20:33:00 +0200

    -

--- Blog/settings.py
+++ Blog/settings.py
@@ -165,7 +165,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'utils.DjsManifestStaticFilesStorage'
+STATICFILES_STORAGE = '.utils.DjsManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
