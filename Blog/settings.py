055c0b34fba96de1ceb2da11b921432f3b58ba0c
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-11 08:25:49 +0200

    -

--- Blog/settings.py
+++ Blog/settings.py
@@ -167,7 +167,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
