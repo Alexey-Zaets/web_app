d9c4cae9806aef47e2c01d3f82d47ac58c871585
Alexey-Zaets <3aets.dev@gmail.com>
2018-11-13 11:33:27 +0200

    update settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['lit-gorge-59056.herokuapp.com', 'www.code4.pp.ua']
+ALLOWED_HOSTS = ['lit-gorge-59056.herokuapp.com']
 
 
 # Application definition
