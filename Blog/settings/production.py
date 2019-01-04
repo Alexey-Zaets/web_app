f97e72c12c54a5abf293a0dc8be05e78df07e147
Alexey-Zaets <3aets.dev@gmail.com>
2019-01-04 16:05:33 +0200

    update settings

--- Blog/settings/production.py
+++ Blog/settings/production.py
@@ -205,4 +205,10 @@ LOGGING = {
     },
 }
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+
 django_heroku.settings(locals())
