17038e04c89bd8b8a1c807560b63a8e0bdc3f278
Alexey-Zaets <3aets.dev@gmail.com>
2019-01-06 14:40:31 +0200

    refactoring

--- subscribers/tasks.py
+++ subscribers/tasks.py
@@ -1,5 +1,6 @@
 import smtplib
 from Blog.celery import app
+from Blog.settings import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 from blog.models import Post
 
 
@@ -8,8 +9,8 @@ def mailing(email, post_id):
 	post = Post.objects.get(id=post_id)
 	fadd = 'djangotest2019@gmail.com'
 	tadd = email
-	username = 'djangotest2019@gmail.com'
-	password = 'testdjangoaccount'
+	username = EMAIL_HOST_USER
+	password = EMAIL_HOST_PASSWORD
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.ehlo()
 	server.starttls()
